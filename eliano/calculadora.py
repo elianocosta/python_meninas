845db1983a676b072cecadedb025bd56213eee6e
elianocosta <eliano.costa@edu.unifor.br>
2022-10-06 20:39:26 -0300

    adicionando calculadorameninas

---
+++ eliano/calculadora.py
@@ -0,0 +1,22 @@
+a=8
+b=2
+
+def soma():
+	a = int(input("Qual o seu primeiro numero? "))
+	b = int(input("Qual o segundo numero? ")	)
+	return a + b
+def subtracao():
+	a = int(input("Qual o seu primeiro numero? "))
+	b = int(input("Qual o segundo numero? "))
+	return a-b	
+def multiplicacao():
+	a = int(input("Qual o seu primeiro numero? "))
+	b = int(input("Qual o segundo numero? "))
+	return a*b
+def divisao():
+	a = int(input("Qual o seu primeiro numero? "))
+	b = int(input("Qual o segundo numero? "))
+	return a/b
+
+print(multiplicacao())
+
